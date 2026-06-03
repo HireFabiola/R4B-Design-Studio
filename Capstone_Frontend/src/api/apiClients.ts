a1c65547fe4600bd35f672aba60cb53a0eae61e1
Fabiola <fabiola.aurelien@gmail.com>
2026-06-03 16:34:53 -0400

    Configured axios client to attach JWT token to requests

--- Capstone_Frontend/src/api/apiClients.ts
+++ Capstone_Frontend/src/api/apiClients.ts
@@ -1,10 +1,23 @@
-// Import and configure Axios for API calls
+// Import axios for making HTTP requests
 import axios from "axios";
 
-// Create an Axios instance with the base URL set to the API URL defined in the environment variables
+// Create an axios instance with a base URL from environment variables and set up an interceptor to include the authentication token in the headers of each request if it exists in localStorage
 const apiClient = axios.create({
   baseURL: import.meta.env.VITE_API_URL,
 });
 
-//  Add a request interceptor to include the authentication token in the headers of each request if it exists in localStorage
+//  
+apiClient.interceptors.request.use((config) => {
+  const token = localStorage.getItem("token");
+
+  //    
+  if (token) {
+    config.headers.Authorization = `Bearer ${token}`;
+  }
+
+// Return the modified config to be used in the request
+  return config;
+});
+
+// Export the configured axios instance for use in making API requests throughout the application
 export default apiClient;
