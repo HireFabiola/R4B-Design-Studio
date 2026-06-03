5d210a7f94a65f60ea692c71e871903815b0f095
Fabiola <fabiola.aurelien@gmail.com>
2026-06-03 17:52:00 -0400

    Added TaskPage code

--- Capstone_Frontend/src/pages/TasksPage.tsx
+++ Capstone_Frontend/src/pages/TasksPage.tsx
@@ -1,5 +1,49 @@
-const TaskPage = () => {
-  return <h1>Task Page</h1>;
+import { useEffect, useState } from "react";
+import { getTasks } from "../services/taskService";
+import type { Task } from "../types/Task";
+
+const TasksPage = () => {
+  const [tasks, setTasks] = useState<Task[]>([]);
+  const [isLoading, setIsLoading] = useState(true);
+  const [error, setError] = useState("");
+
+  useEffect(() => {
+    const fetchTasks = async () => {
+      try {
+        const data = await getTasks();
+        setTasks(data);
+      } catch {
+        setError("Unable to load tasks.");
+      } finally {
+        setIsLoading(false);
+      }
+    };
+
+    fetchTasks();
+  }, []);
+
+  if (isLoading) return <p>Loading tasks...</p>;
+  if (error) return <p>{error}</p>;
+
+  return (
+    <section>
+      <h1>Tasks</h1>
+
+      {tasks.length === 0 ? (
+        <p>No tasks yet.</p>
+      ) : (
+        <ul>
+          {tasks.map((task) => (
+            <li key={task._id}>
+              <h2>{task.title}</h2>
+              <p>{task.status}</p>
+              <p>{task.description}</p>
+            </li>
+          ))}
+        </ul>
+      )}
+    </section>
+  );
 };
 
-export default TaskPage;
+export default TasksPage;
